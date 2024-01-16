0100f7a8dc61d768f96db002b15512507f8d0086
Kulith <kulithvinwara119@gmail.com>
2024-01-16 13:09:26 +0530

    front end modifications

--- script.js
+++ script.js
@@ -1,22 +1,55 @@
-let menu = document.querySelector("#navbar");
-let nav = document.querySelector(".menu");
+const formOpenBtn = document.querySelector("#form-open"),
+  home = document.querySelector(".home"),
+  formContainer = document.querySelector(".form_container"),
+  formCloseBtn = document.querySelector(".form_close"),
+  signupBtn = document.querySelector("#signup"),
+  loginBtn = document.querySelector("#login"),
+  pwShowHide = document.querySelectorAll(".pw_hide");
+  
+pwShowHide.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    let getPwInput = icon.parentElement.querySelector("input");
+    if (getPwInput.type === "password") {
+      getPwInput.type = "text";
+      icon.classList.replace("uil-eye-slash", "uil-eye");
+    } else {
+      getPwInput.type = "password";
+      icon.classList.replace("uil-eye", "uil-eye-slash");
+    }
+  });
+});
 
-let showcontactform = document.querySelector(".contact");
-let myform = document.querySelector(".contact-form")
+signupBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    update();
+    home.classList.remove("show");
+    formContainer.classList.add("active"); 
+    document.body.classList.add("adjusted");
+  });
 
-let formclose = document.querySelector("#form-close")
+loginBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  formContainer.classList.remove("active"); 
+  home.classList.add("show");
+  document.body.classList.remove("adjusted");
+});
 
-
-
-menu.onclick = () =>{
-    menu.classList.toggle("fa-times")
-    nav.classList.toggle("active")
+function update() {
+  var selectedRole = document.getElementById("userRole").value;
+  switch (selectedRole) {
+    case "parent":
+      document.getElementById("emailInput").style.display = "block";
+      document.getElementById("passwordInput").style.display = "block";
+      document.getElementById("confirmPasswordInput").style.display = "block";
+      break;
+    case "owner":
+      document.getElementById("cityInput").style.display = "none";
+      break;
+    case "driver":
+      document.getElementById("cityInput").style.display = "none";
+      break;
+    case "admin":
+      // Handle admin specific fields
+      break;
+  }
 }
-showcontactform.onclick = () =>{
-    myform.classList.toggle('active')
-}
-formclose.onclick = () =>{
-    myform.classList.remove('active')
-}
-
-
